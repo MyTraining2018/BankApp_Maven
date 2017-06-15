00889342b8890a9bafda1d343353883a6e2c8940
Training2017 <captrainingfeb2016@gmail.com>
2017-06-15 18:53:37 +0530

    Commit

---
+++ Day1-BankApp/src/test/java/org/cap/bankapp/test/RulesTestCase.java
@@ -0,0 +1,29 @@
+package org.cap.bankapp.test;
+
+import org.cap.dto.Customer;
+import org.cap.exception.InvalidInitialAmountException;
+import org.cap.service.AcccountService;
+import org.cap.service.AccountServiceImpl;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class RulesTestCase {
+	
+
+	@Rule
+	public ExpectedException thrown=ExpectedException.none();
+	
+	@Test
+	public void test_addAccount_with_nullCustomer() throws InvalidInitialAmountException{
+		AcccountService	acccountService=new AccountServiceImpl();
+		Customer customer=null;
+		
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Invalid Customer.");
+		
+		
+		acccountService.addAccount(customer, 1000);
+		
+	}
+}
